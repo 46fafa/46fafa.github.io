eee7d3f42ea457c6c5b63701ff0cf83147b0e5c9
kenome <test@gmail>
2025-01-01 21:04:34 +0800

    auto deploy

--- auto_deploy.bat
+++ auto_deploy.bat
@@ -1,7 +1,14 @@
 @echo off
-chcp 65001
-REM 定义目标文件夹变量
-set "TARGET_FOLDER=D:\blog\public"
+
+
+REM 检查目录切换是否成功
+if %ERRORLEVEL% neq 0 (
+    echo 目录切换失败，错误码：%ERRORLEVEL%
+    pause
+    exit /b %ERRORLEVEL%
+) else (
+    echo 目录切换成功
+)
 
 REM 运行 Hugo 命令
 echo 正在运行 Hugo 命令...
@@ -14,20 +21,10 @@ if %ERRORLEVEL% neq 0 (
     echo Hugo 命令执行成功
 )
 
-REM 切换到目标文件夹
-cd /d "%TARGET_FOLDER%"
-
-REM 检查目录切换是否成功
-if %ERRORLEVEL% neq 0 (
-    echo 目录切换失败，错误码：%ERRORLEVEL%
-    pause
-    exit /b %ERRORLEVEL%
-) else (
-    echo 目录切换成功
-)
-
 REM 运行 Git 命令
 echo 正在运行 Git 命令...
+
+REM 添加所有更改到暂存区
 git add .
 if %ERRORLEVEL% neq 0 (
     echo git add 命令执行失败，错误码：%ERRORLEVEL%
@@ -37,7 +34,8 @@ if %ERRORLEVEL% neq 0 (
     echo git add 命令执行成功
 )
 
-git commit -a -m "auto deploy"
+REM 提交更改
+git commit -m "auto deploy"
 if %ERRORLEVEL% neq 0 (
     echo git commit 命令执行失败，错误码：%ERRORLEVEL%
     pause
@@ -46,7 +44,8 @@ if %ERRORLEVEL% neq 0 (
     echo git commit 命令执行成功
 )
 
-git push --set-upstream origin main
+REM 推送到远程仓库
+git push
 if %ERRORLEVEL% neq 0 (
     echo git push 命令执行失败，错误码：%ERRORLEVEL%
     pause
@@ -55,7 +54,8 @@ if %ERRORLEVEL% neq 0 (
     echo git push 命令执行成功
 )
 
-REM 等待5秒后关闭弹窗
-echo 操作完成，5秒后关闭窗口...
-timeout /t 5 /nobreak >nul
+REM 等待用户操作
+pause
+
+REM 结束脚本
 exit
